b4df2e76e26c0bb712ca44053c0a3d08b46d4624
deepthedeata <98915818+deepthedeata@users.noreply.github.com>
2026-05-15 12:10:30 +0530

    Delete src directory

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import { installMockApi } from './mockApi';
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
-// Install mock API BEFORE app renders.
-installMockApi();
-
-createRoot(document.getElementById("root")!).render(<App />);
